f6ca9e0b66898602b188b7894dc8650132322346
yyyifffang <Fragrant115@gmail.com>
2022-12-29 17:28:24 +0800

    final 2

---
+++ app/src/main/java/com/example/afinal/ExplainActivity.java
@@ -0,0 +1,12 @@
+package com.example.afinal;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ExplainActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.explain);
+    }
+}
--- app/src/main/java/com/example/afinal/MainActivity.java
+++ app/src/main/java/com/example/afinal/MainActivity.java
@@ -1,112 +1,73 @@
 package com.example.afinal;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.content.pm.ActivityInfo;
+import android.app.Activity;
+import android.app.Service;
+import android.content.Context;
+import android.content.Intent;
+import android.media.MediaPlayer;
 import android.os.Bundle;
-import android.os.Handler;
-import android.os.Message;
-import android.util.Log;
-import android.view.MotionEvent;
+import android.os.IBinder;
 import android.view.View;
-import android.view.WindowManager;
-import android.widget.ImageView;
-import android.widget.TextView;
+import android.widget.Button;
 import android.widget.Toast;
 
-import java.util.Random;
 
-public class MainActivity extends AppCompatActivity {
-    //1.定義變量、對象、洞穴座標
-    private int i=0; //紀錄打到的地鼠個數
-    private ImageView mouse; //定義滑鼠
-    private TextView info1; //用於查看洞穴座標
-    private Handler handler;
-    public int[][] position = new int[][]{
-            {396,100},{780,100},{1134,100},
-            {350,370},{780,370},{1190,370},
-            {297,650},{780,650},{1240,650}};//地鼠位置的陣列
+public class MainActivity extends Activity {
+
+    private Button startGame;
+    private Button continueGame;
+    private Button explain;
+    private Button exit;
+
+
+    public final Context TAG = MainActivity.this;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initView(); //初始化控件
+        initListener(); //初始化监听器
 
-        getWindow().setFlags(WindowManager. LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
-        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE); //設置橫屏
-
-        //2.綁定元件
-        mouse = (ImageView)findViewById(R.id.imageView);
-        info1=findViewById(R.id.info);
-        //獲得洞穴位置
-        info1.setOnTouchListener(new View.OnTouchListener() {
-            @Override
-            public boolean onTouch(View v, MotionEvent event) {
-                switch (event.getAction()) {
-                    case MotionEvent.ACTION_DOWN:
-                        float x =event.getRawX();
-                        float y =event.getRawY();
-                        Log.i("x:"+x,"y"+y);
-                        break;
-                    default:
-                        break;
-                }
-                return false;
-            }
-        });
-
-
-
-
-
-
-        //3.地鼠隨機出現
-        //創建handler消息處理機制
-        //創建線程
-        Handler handler = new Handler(){
-            @Override
-            public void handleMessage(@NonNull Message msg){
-                int index;
-                if(msg.what == 0x101) {
-                    index = msg.arg1;
-                    mouse.setX(position[index][0]);
-                    mouse.setY(position[index][1]);
-                    mouse.setVisibility(View.VISIBLE);
-                }
-                super.handleMessage(msg);
-            }
-        };
-        //創建線程
-        Thread t =new Thread(new Runnable() {
-            @Override
-            public void run() {
-                int index=0; //紀錄地鼠位置
-                while (!Thread.currentThread().isInterrupted()) { //當前進度不中斷的時候
-                    index = new Random().nextInt(position.length); //生產一個隨機整數
-                    Message m = handler.obtainMessage(); //創建消息對象
-                    m.what = 0x101; //設置消息標誌
-                    m.arg1 = index; //保存地鼠位置的索引值
-                    handler.sendMessage(m); //發送消息通知handler處理
-                    try{
-                        Thread.sleep(new Random().nextInt(500)+500); //休息一段時間
-                    }catch (InterruptedException e) {
-                        e.printStackTrace();
-                    }
-                }
-            }
-        });
-        t.start();
-        //4.打到地鼠後，地鼠顯示&不顯示
-        //添加點擊滑鼠後的事件
-        mouse.setOnTouchListener(new View.OnTouchListener() {
-            @Override
-            public boolean onTouch(View v, MotionEvent event) {
-                v.setVisibility(View.INVISIBLE); //設定地鼠不顯示
-                i++;
-                Toast.makeText(MainActivity.this,"打到"+i+"隻地鼠了！",Toast.LENGTH_LONG).show();
-                return false;
-            }
-        });
     }
+
+    private void initListener() {
+        BtnListener listener = new BtnListener();
+        startGame.setOnClickListener(listener);
+        continueGame.setOnClickListener(listener);
+        explain.setOnClickListener(listener);
+        exit.setOnClickListener(listener);
+    }
+
+    private void initView() {
+
+        startGame = (Button) findViewById(R.id.btn_start);
+        continueGame = (Button) findViewById(R.id.btn_continue);
+        explain = (Button) findViewById(R.id.btn_explain);
+        exit = (Button) findViewById(R.id.btn_exit);
+    }
+
+    class BtnListener implements View.OnClickListener {
+
+        @Override
+        public void onClick(View v) {
+            switch (v.getId()) {
+                case R.id.btn_start:
+                    Intent intent1 = new Intent(TAG, GameActivity.class);
+                    startActivity(intent1);
+                    break;
+                case R.id.btn_continue:
+                    Intent intent2 = new Intent(TAG, GameActivity.class);
+                    startActivity(intent2);
+                    break;
+                case R.id.btn_explain:
+                    Intent intent3 = new Intent(TAG, ExplainActivity.class);
+                    startActivity(intent3);
+                    break;
+                case R.id.btn_exit:
+                    finish();
+                    break;
+            }        }
+    }
+
 }
